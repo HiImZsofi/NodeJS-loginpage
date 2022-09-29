67eb57d5b6c21a5744c23d57c936c9e6d61aa061
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-29 12:44:29 +0200

    initialize express modules, declare login route

---
+++ login.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql');
+const express = require('express');
+const session = require('express-session');  
+const path = require('path');
+
+const connection = mysql.createConnection({  //default database connection
+    host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'nodelogin',
+})
+
+const app = express(); //initializes express
+
+app.use(session({           //express module configuration
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());  //express json extracts data from the html page
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, '/login.html'));     //declare the login route
+})
+
+
